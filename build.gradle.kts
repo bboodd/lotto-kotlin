da43d5947b0c0d6d2bcc5dd4335260df9f606195
bboodd <gkrgus9@gmail.com>
2026-05-28 18:41:38 +0900

    [config] reflection 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,7 @@ dependencies {
     testImplementation(kotlin("test"))
     testImplementation("io.kotest:kotest-runner-junit5:6.1.11")
     testImplementation("io.kotest:kotest-assertions-core:6.1.11")
+    implementation(kotlin("reflect"))
 }
 
 tasks.withType<Test>().configureEach {
